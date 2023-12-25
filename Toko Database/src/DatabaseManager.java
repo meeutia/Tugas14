442121867b2b369c1edd914fd326a3c6e74625a3
meeutia <145307246+meeutia@users.noreply.github.com>
2023-12-25 20:58:59 +0700

    Add files via upload

--- Toko Database/src/DatabaseManager.java
+++ Toko Database/src/DatabaseManager.java
@@ -1,4 +1,6 @@
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class DatabaseManager {
@@ -13,6 +15,7 @@ public class DatabaseManager {
         return DriverManager.getConnection(DATABASE_URL, connectionProps);
     }
 
+    // CREATE
     public static void createPurchaseRecord(String namaPelanggan, String noHpPelanggan, String alamatPelanggan, long totalBayar) {
         Connection connection = null;
 
@@ -21,8 +24,8 @@ public class DatabaseManager {
             connection.setAutoCommit(false);  // Non-otomatisasi transaksi
 
             try (PreparedStatement preparedStatement = connection.prepareStatement(
-                     "INSERT INTO purchase_records (nama_pelanggan, no_hp_pelanggan, alamat_pelanggan, total_bayar) VALUES (?, ?, ?, ?)",
-                     Statement.RETURN_GENERATED_KEYS)) {
+                    "INSERT INTO purchase_records (nama_pelanggan, no_hp_pelanggan, alamat_pelanggan, total_bayar) VALUES (?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS)) {
                 preparedStatement.setString(1, namaPelanggan);
                 preparedStatement.setString(2, noHpPelanggan);
                 preparedStatement.setString(3, alamatPelanggan);
@@ -62,6 +65,72 @@ public class DatabaseManager {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    // READ
+    public static List<PurchaseRecord> getAllPurchaseRecords() {
+        List<PurchaseRecord> purchaseRecords = new ArrayList<>();
+
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM purchase_records")) {
+            while (resultSet.next()) {
+                PurchaseRecord purchaseRecord = new PurchaseRecord(
+                        resultSet.getLong("id"),
+                        resultSet.getString("nama_pelanggan"),
+                        resultSet.getString("no_hp_pelanggan"),
+                        resultSet.getString("alamat_pelanggan"),
+                        resultSet.getLong("total_bayar")
+                );
+                purchaseRecords.add(purchaseRecord);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return purchaseRecords;
+    }
+
+    // UPDATE
+    public static void updatePurchaseRecord(long id, String namaPelanggan, String noHpPelanggan, String alamatPelanggan, long totalBayar) {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "UPDATE purchase_records SET nama_pelanggan=?, no_hp_pelanggan=?, alamat_pelanggan=?, total_bayar=? WHERE id=?")) {
+            preparedStatement.setString(1, namaPelanggan);
+            preparedStatement.setString(2, noHpPelanggan);
+            preparedStatement.setString(3, alamatPelanggan);
+            preparedStatement.setLong(4, totalBayar);
+            preparedStatement.setLong(5, id);
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows == 0) {
+                System.out.println("No records updated.");
+            } else {
+                System.out.println("Purchase record with ID " + id + " updated successfully.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // DELETE
+    public static void deletePurchaseRecord(long id) {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "DELETE FROM purchase_records WHERE id=?")) {
+            preparedStatement.setLong(1, id);
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows == 0) {
+                System.out.println("No records deleted.");
+            } else {
+                System.out.println("Purchase record with ID " + id + " deleted successfully.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
